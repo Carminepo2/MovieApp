eefcd3601b58fd17456f02a9e6e08bd9aa699432
Carmine Porricelli <carmineporricelli96@gmail.com>
2022-02-12 17:22:34 +0100

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Carmine Porricelli on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
