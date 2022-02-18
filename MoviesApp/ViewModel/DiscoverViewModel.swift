d8e3ac587bb35bfd0bf0c9efbdd3286656179f1b
riccardoruocco <ruocco.rf@gmail.com>
2022-02-18 17:31:34 +0100

    toDiscard

--- MoviesApp/ViewModel/DiscoverViewModel.swift
+++ MoviesApp/ViewModel/DiscoverViewModel.swift
@@ -7,17 +7,18 @@
 
 import Foundation
 
+
 class DiscoverViewModel: ObservableObject {
-    @Published var movieCards: Array<MovieCard> = []
+    @Published var movieCards: Array<MovieCard>
     @Published var rotationDegreeCards: Array<Double> = []
     @Published var model:MovieAppModel = MovieAppModel.shared
     var advisor:GrandAdvisor = GrandAdvisor.shared
     
     init() {
-        setCards()
+        setModel()
     }
     
-    func setCards(){
+    func setModel(){
         for _ in 0..<Constants.NumOfCards {
             movieCards.append(MovieCard(movie: self.getAdvice()))
         }
